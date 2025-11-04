eea4f1deab4f79c8b2ca6f60d0d6c772b754ad2a
Divarizky <divarizky28@gmail.com>
2025-11-04 20:55:22 +0700

    feature: menangani aksi tombol Follow dan Message - Tampilkan pesan pop-up ‘Pesan telah dikirim ke [Nama Pengguna]’ saat tombol Pesan diklik. - Ubah teks tombol ‘Follow’ menjadi ‘Following’ setelah diklik.

--- app/src/main/java/com/divarizky/profilecardapp/MainActivity.kt
+++ app/src/main/java/com/divarizky/profilecardapp/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.divarizky.profilecardapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var isFollowing = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnFollow = findViewById<Button>(R.id.btnFollow)
+        val btnMessage = findViewById<Button>(R.id.btnMessage)
+        val textName = findViewById<TextView>(R.id.fullName)
+
+        // merubah kondisi tombol "Follow" ketika di tekan
+        btnFollow.setOnClickListener {
+            isFollowing = !isFollowing
+            btnFollow.text = if (isFollowing) "Following" else "Follow"
+        }
+
+        // menampilkan pesan pop-up terkirim ketika tombol "Message" di tekan
+        btnMessage.setOnClickListener {
+            Toast.makeText(this, "Pesan terkirim ke ${textName.text}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
